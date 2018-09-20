89e577403326930e39773ef7eb2406bf3c0a214f
Ébano Penha Andrello Lopes <ebano@onbi.com.br>
2018-09-20 11:41:22 -0300

    Improvements on observers: performance and avoid exceptions to users Added try .. catch on methods to prevent exceptions to users Now checking if configurations have values before call API Removed unnecessary model loads (order and product) PSR-2

--- app/code/community/Anymarket/ApiExtension/Model/Observer.php
+++ app/code/community/Anymarket/ApiExtension/Model/Observer.php
@@ -2,73 +2,89 @@
 
 class Anymarket_ApiExtension_Model_Observer
 {
-
+    /**
+     * Perform calls to anymarket API
+     *
+     * @param $host
+     * @return array
+     */
     private function doCallAnymarket($host)
     {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $host,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "",
-            CURLOPT_HTTPHEADER => array(
-                "cache-control: no-cache",
-                "content-type: application/json"
-            ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-        if ($err) {
-            return array("error" => "1", "message" => $err);
-        } else {
-            return array("error" => "0", "message" => $response);
+        try {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $host,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => "",
+                CURLOPT_HTTPHEADER => array(
+                    "cache-control: no-cache",
+                    "content-type: application/json"
+                ),
+            ));
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+
+            if ($err) {
+                return array("error" => "1", "message" => $err);
+            } else {
+                return array("error" => "0", "message" => $response);
+            }
+        } catch (Exception $e) {
+            Mage::logException($e);
         }
     }
 
     /**
      * @param $observer
-     *
-     * @return $this
      */
     public function updateOrderAnyMarketObs($observer)
     {
-        if ($observer->getEvent()->getOrder()) {
-            $storeID = $observer->getEvent()->getOrder()->getStoreId();
-            $OrderID = $observer->getEvent()->getOrder()->getIncrementId();
-            $order = new Mage_Sales_Model_Order();
-            $order->loadByIncrementId($OrderID);
+        try {
+            if ($observer->getEvent()->getOrder()) {
+                $storeId = $observer->getEvent()->getOrder()->getStoreId();
+                $incrementId = $observer->getEvent()->getOrder()->getIncrementId();
 
-            $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeID);
-            $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeID);
+                $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeId);
+                $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeId);
 
-            $host = $host . "/public/api/anymarketcallback/order/" . $oi . "/MAGENTO_1/" . $storeID . "/" . $OrderID;
-            $this->doCallAnymarket($host);
+                if (empty($host) == false && empty($oi) == false) {
+                    $host = $host . "/public/api/anymarketcallback/order/" . $oi . "/MAGENTO_1/" . $storeId . "/" . $incrementId;
+                    $this->doCallAnymarket($host);
+                }
+            }
+        } catch (Exception $e) {
+            Mage::logException($e);
         }
     }
 
-
     /**
      * @param $observer
-     * @return array
      */
     public function sendProdAnyMarket($observer)
     {
-        if ($observer->getEvent()->getProduct()) {
-            $productId = $observer->getEvent()->getProduct()->getId();
-            $storeID = $observer->getEvent()->getProduct()->getStoreId();
+        try {
+            if ($observer->getEvent()->getProduct()) {
+                $productId = $observer->getEvent()->getProduct()->getId();
+                $storeId = $observer->getEvent()->getProduct()->getStoreId();
 
-            $product = Mage::getModel('catalog/product')->setStoreId($storeID)->load($productId);
+                $sku = $this->getProductSku($productId, $storeId);
 
-            $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeID);
-            $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeID);
+                $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeId);
+                $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeId);
 
-            $host = $host . "/public/api/anymarketcallback/product/" . $oi . "/MAGENTO_1/" . $storeID . "/" . $product->getSku();
-            $this->doCallAnymarket($host);
+                if (empty($host) == false && empty($oi) == false) {
+                    $host = $host . "/public/api/anymarketcallback/product/" . $oi . "/MAGENTO_1/" . $storeId . "/" . $sku;
+                    $this->doCallAnymarket($host);
+                }
+            }
+        } catch (Exception $e) {
+            Mage::logException($e);
         }
     }
 
@@ -78,29 +94,60 @@ class Anymarket_ApiExtension_Model_Observer
      */
     public function catalogInventorySave($observer)
     {
-        $event = $observer->getEvent();
-        $_item = $event->getItem();
-        $product = Mage::getModel('catalog/product')->load($_item->getProductId());
+        try {
+            $item = $observer->getEvent()->getItem();
 
-        if ($_item->getData('store_id') != null && $_item->getData('store_id') != "0") {
-            $storeID = $_item->getData('store_id');
-            $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeID);
-            $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeID);
-            $host = $host . "/public/api/anymarketcallback/stockPrice/" . $oi . "/" . $product->getSku();
+            if (is_object($item)) {
+                $storeId = intval($item->getData('store_id'));
+            }
 
-            $this->doCallAnymarket($host);
-        } else {
-            foreach ($product->getStoreIds() as $storeID) {
-                $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeID);
-                if ($host != null && $host != "") {
-                    $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeID);
-                    $host = $host . "/public/api/anymarketcallback/stockPrice/" . $oi . "/" . $product->getSku();
+            if ($storeId > 0) {
+                $sku = $this->getProductSku($item->getProductId(), $storeId);
+
+                $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeId);
+                $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeId);
+
+                if (empty($host) == false && empty($oi) == false) {
+                    $host = $host . "/public/api/anymarketcallback/stockPrice/" . $oi . "/" . $sku;
                     $this->doCallAnymarket($host);
                 }
+            } else {
+                $product = Mage::getModel('catalog/product')->load($item->getProductId());
+
+                foreach ($product->getStoreIds() as $storeId) {
+                    $host = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_host_field', $storeId);
+
+                    if (empty($host) == false) {
+                        $oi = Mage::getStoreConfig('anymarket_new_section/anymarket_new_access_group/anymarket_new_oi_field', $storeId);
+
+                        if (empty($oi) == false) {
+                            $host = $host . "/public/api/anymarketcallback/stockPrice/" . $oi . "/" . $product->getSku();
+                            $this->doCallAnymarket($host);
+                        }
+                    }
+                }
             }
+        } catch (Exception $e) {
+            Mage::logException($e);
         }
     }
 
-}
+    /**
+     * Get product SKU without loading product model object
+     *
+     * @param $productId
+     * @param $storeId
+     * @return mixed
+     */
+    protected function getProductSku($productId, $storeId)
+    {
+        $productResource = Mage::getSingleton('catalog/product')->getResource();
+        $sku = $productResource->getAttributeRawValue(
+            $productId,
+            'sku',
+            Mage::app()->getStore($storeId)
+        );
+        return $sku;
+    }
 
-?>
+}
